bacfbd4af85d4a6fbfb5b72c60616770ab654d83
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-07-25 16:17:27 +1000

    Moved contact info into container and adjusted propTypes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 class Contact extends Component {
   render() {
-    const {name, email, phone} = this.props;
+    const {name, email, phone} = this.props.contact;
     return (
       <div className='card card-body mb-3'>
         <h4>{name}</h4>
@@ -17,10 +17,8 @@ class Contact extends Component {
   }
 }
 
-Contact.proppTypes = {
-  name: PropTypes.string.isRequired,
-  email: PropTypes.string.isRequired,
-  phone: PropTypes.string.isRequired
+Contact.propTypes = {
+  contact: PropTypes.object.isRequired
 };
 
 export default Contact;
