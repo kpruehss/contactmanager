ba1eaa4ad70be5744dd6f2c8e443f89d10c6b87e
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-07-26 16:41:24 +1000

    Added fake API POST + DELETE requests to jsonplaceholder

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Consumer } from 'context';
-import uuid from 'uuid';
+import axios from 'axios';
 import TextInputGroup from '../layout/TextInputGroup';
 
 class AddContact extends Component {
@@ -33,12 +33,14 @@ class AddContact extends Component {
     }
 
     const newContact = {
-      id: uuid(),
       name,
       email,
       phone,
     };
-    dispatch({ type: 'ADD_CONTACT', payload: newContact });
+
+    axios
+      .post('https://jsonplaceholder.typicode.com/users', newContact)
+      .then(res => dispatch({ type: 'ADD_CONTACT', payload: res.data }));
 
     // Clear form components
     this.setState({
--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Consumer } from 'context';
+import axios from 'axios';
 
 class Contact extends Component {
   state = {
@@ -13,7 +14,10 @@ class Contact extends Component {
   };
 
   onDeleteClick = (id, dispatch) => {
-    dispatch({ type: 'DELETE_CONTACT', payload: id });
+    // Run a fake delete request to jsonplaceholder, then delete from DOM
+    axios
+      .delete(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then(res => dispatch({ type: 'DELETE_CONTACT', payload: id }));
   };
 
   render() {
